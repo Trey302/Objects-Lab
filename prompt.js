48ad137182d4593687acba387a6d0fcef885116a
Route9Library <you@example.com>
2020-11-30 17:33:15 -0500

    prompt 3, 4, & 5

--- prompt.js
+++ prompt.js
@@ -12,14 +12,21 @@ const car = {
 //    console.log(car)
 
 // Prompt 2: Write a function that takes an object as an argument and prints it to the console.
-function object() {
-for (let i = 0; i > car.length; i++) {
-    console.log(car)
-}
-}
+// function object() {
+// for (let i = 0; i > car.length; i++) {
+//     console.log(car)
+// }
+// }
 
+// Delete the gasoline property from the object. Also print the object before or after deleting the property.
+// delete car.gasoline;
+// console.log(car);
 
+// Write a function that returns the object keys as an array.
+// const propertyKeys = Object.keys(car);
+// console.log(propertyKeys);
 
+// Write a function that returns the object's values as an array.
+const propertyValues = Object.values(car);
 
-
-
+console.log(propertyValues);
