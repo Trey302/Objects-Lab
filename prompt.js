fda4e36c1d4ebd547b4f23f3d8b78c0654025fa7
Route9Library <you@example.com>
2020-11-30 16:53:47 -0500

    prompt 1 & 2

---
+++ prompt.js
@@ -0,0 +1,25 @@
+const car = {
+    make : "ford",
+    model : "mustang",
+    color : "black",
+    type : "sedan",
+    tires : "firestone",
+    mode : "sport",
+    gasoline : "super"
+   }
+
+// Prompt 1: Print the object above to the console.
+//    console.log(car)
+
+// Prompt 2: Write a function that takes an object as an argument and prints it to the console.
+function object() {
+for (let i = 0; i > car.length; i++) {
+    console.log(car)
+}
+}
+
+
+
+
+
+
